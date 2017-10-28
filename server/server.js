f0add4683a965f89e7ce9390fc587ef9a4ea348e
Shihab Jamal <shihab_jamal@yahoo.com>
2017-10-28 23:01:01 +1100

    Add GET/todos/:id

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+const {ObjectID} = require('mongodb');
 
 var app = express();
 
@@ -27,6 +28,24 @@ app.get('/todos', (req, res) => {
     }, (error) => {
         res.status(400).send(error);
     } );
+});
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if (!ObjectID.isValid(id))
+    {
+        return res.status(404).send();
+    }
+    
+    Todo.findById(id).then( (todo) => {
+        if (!todo)
+        {
+            return res.status(404).send();
+        }
+
+        res.send({todo});
+    }).catch((error) => res.status(400).send());
 });
 
 app.listen(3000, ()=> {

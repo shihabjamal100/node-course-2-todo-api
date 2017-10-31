07e8aa7a55c73bd1a9b7163699b5a93adee3c53d
Shihab Jamal <shihab_jamal@yahoo.com>
2017-10-31 21:56:20 +1100

    Set up separate and test environments

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+require('./config/config');
+
 const _ = require('lodash');
 
 const express= require('express');
@@ -10,7 +12,7 @@ const {ObjectID} = require('mongodb');
 
 var app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(bodyParser.json())
 

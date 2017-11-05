7a7172b30e88adf3efe9b681afb053b9a2d737e0
Shihab Jamal <shihab_jamal@yahoo.com>
2017-11-05 18:34:37 +1100

    Added POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -86,6 +86,31 @@ UserSchema.statics.findByToken = function (token) {    // this is a static metho
     });
 }
 
+UserSchema.statics.findByCredentials = function(email, password) {
+    var User = this;
+    
+    return User.findOne({email}).then( (user) => {
+        if (!user)
+        {
+            return Promise.reject();
+        }
+
+        return new Promise( (resolve, reject) => {
+            bcrypt.compare(password, user.password, (error, result) => {                
+                if (result)
+                {
+                    resolve(user);
+                }
+                else
+                {
+                    reject(error);
+                }
+                    
+            })
+        });
+    });
+}
+
 // See Mongoose middleware documentation.
 UserSchema.pre('save', function (next) {
     var user = this;

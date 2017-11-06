77aaed49afa706b5a5ea46761a43c42691f1ecdc
Shihab Jamal <shihab_jamal@yahoo.com>
2017-11-06 23:23:53 +1100

    Update config to useconfig.json file

--- server/config/config.js
+++ server/config/config.js
@@ -1,6 +1,16 @@
 var env = process.env.NODE_ENV || 'development';
 
-if (env === 'development')
+if (env === 'development' || env === 'test')
+{
+    var config = require('./config.json');
+    var envConfig = config[env];
+
+    var keys = Object.keys(envConfig).forEach((key) => {
+        process.env[key] = envConfig[key];
+    })
+}
+
+/* if (env === 'development')
 {
     process.env.PORT = 3000;
     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
@@ -9,5 +19,5 @@ else if (env === 'test')
 {
     process.env.PORT = 3000;
     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
-}
+} */
 //else in Heroku the env variable is set to production
--- server/models/user.js
+++ server/models/user.js
@@ -53,7 +53,7 @@ UserSchema.methods.generateAuthToken = function () {
     var token = jwt.sign({
         _id: user._id.toHexString(), 
         access
-    }, 'abc123').toString();
+    }, process.env.JWT_SECRET).toString();
 
     user.tokens.push({access, token});
 
@@ -78,7 +78,7 @@ UserSchema.statics.findByToken = function (token) {    // this is a static metho
 
     try
     {
-        decoded = jwt.verify(token, 'abc123');
+        decoded = jwt.verify(token, process.env.JWT_SECRET);
     }
     catch(e)
     {
